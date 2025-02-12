b88199e08a097084ec29b66ef0f74ff816bedbba
Piotr Mieszczak <mieszczak.piotr@gmail.com>
2025-02-12 03:00:19 +0100

    chore: generate ds lib

---
+++ libs/src/lib/design-system/design-system.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-design-system',
+  imports: [CommonModule],
+  templateUrl: './design-system.component.html',
+  styleUrl: './design-system.component.scss',
+})
+export class DesignSystemComponent {}
