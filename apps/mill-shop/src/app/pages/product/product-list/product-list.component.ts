552b000c39b42554207caacbcf186992fede5dca
Piotr Mieszczak <piotr.mieszczak@gmail.com>
2025-02-15 22:53:56 +0100

    feat: product list component

--- apps/mill-shop/src/app/pages/product/product-list/product-list.component.ts
+++ apps/mill-shop/src/app/pages/product/product-list/product-list.component.ts
@@ -1,17 +1,23 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductFacadeService } from '../../../domain/product/facade';
+import { ClickboxComponent, LinkComponent } from '@mill-shop/design-system/components';
+import { DomSanitizer } from '@angular/platform-browser';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   templateUrl: './product-list.component.html',
-  imports: [RouterModule],
+  styleUrl: './product-list.component.scss',
+  imports: [ClickboxComponent, CurrencyPipe, LinkComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductListComponent implements OnInit {
   private productFacade = inject(ProductFacadeService);
-  private route = inject(ActivatedRoute);
+  private activeRouter = inject(ActivatedRoute);
+  private router = inject(Router);
+  private sanitizer = inject(DomSanitizer);
 
   categorySlug = signal('');
   productsSignal = this.productFacade.productsSignal;
@@ -20,7 +26,15 @@ export class ProductListComponent implements OnInit {
   categorySignal = this.productFacade.categorySignal;
 
   ngOnInit(): void {
-    this.categorySlug.set(this.route.snapshot.paramMap.get('slug') ?? '');
+    this.categorySlug.set(this.activeRouter.snapshot.paramMap.get('slug') ?? '');
     this.productFacade.getProductsByCategory(this.categorySlug());
+  }
+
+  backgroundImageSignal(imageUrl: string) {
+    return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+  }
+
+  onClick(productSlug: string) {
+    this.router.navigate(['/categories', this.categorySignal()?.slug, 'product', productSlug]);
   }
 }
