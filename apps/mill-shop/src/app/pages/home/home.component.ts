b645aa6bab78381f17db347a8b3570759e6d942d
Piotr Mieszczak <piotr.mieszczak@gmail.com>
2025-02-15 18:51:07 +0100

    feat: initial home page

--- apps/mill-shop/src/app/pages/home/home.component.ts
+++ apps/mill-shop/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { HomeFacadeService } from '../../domain/home/facade';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
@@ -13,8 +14,9 @@ export class HomeComponent {
   homePageSignal = this.facade.homePageSignal;
   loadingSignal = this.facade.loadingSignal;
   errorSignal = this.facade.errorSignal;
-
+  backgroundImageSignal = computed(() => `url(${this.homePageSignal()?.banner.url})`);
   constructor() {
     this.facade.loadHomePage();
+    console.log('homePageSignal', this.homePageSignal());
   }
 }
